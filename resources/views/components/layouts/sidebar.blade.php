746d3915b7f84cf99d95326d97d32a019a8ad436
Yukie-Billal <yukiembillal01@gmail.com>
2023-03-10 15:10:07 +0700

    perbaikan sidebar

--- resources/views/components/layouts/sidebar.blade.php
+++ resources/views/components/layouts/sidebar.blade.php
@@ -7,12 +7,38 @@
 	</div>
 	<div class="sidebar-body">	
 		<div class="sidebar-menu d-flex flex-column align-items-start">
-			<div class="menu" data-bs-toggle="collapse" data-bs-target="#dataSubMenu">
+			<div class="menu" data-bs-toggle="collapse" data-bs-target="#dataMaster">
 				<img src="{{ asset('icon/data.png') }}" alt=".." class="me-2" style="width: 20px; height: 20px;">
 				<span>Data Master</span>
 				<i class="fa-solid fa-chevron-down"></i>
 			</div>
-			<div class="collapse w-100 {{ Request::is('barangs') || Request::is('suppliers-users') || Request::is('barang-masuks') || Request::is('pinjams-kembalis*') ? 'show' : '' }}" id="dataSubMenu">
+			<div class="collapse w-100 {{ Request::is('barangs') || Request::is('suppliers-users') || Request::is('barang-masuks') || Request::is('pinjams-kembalis*') ? 'show' : '' }}" id="dataMaster">
+				<div class="sub-menu">						
+					<a class="{{ Request::is('suppliers-users*') ? 'active' : '' }}" href="/siswas">Siswa</a>
+					<a href="/pinjams-kembalis" class="{{ Request::is('pinjams-kembalis*') ? 'active' : '' }}">Pinjam & Kembali</a>
+				</div>
+			</div>
+		</div>
+		<div class="sidebar-menu d-flex flex-column align-items-start">
+			<div class="menu" data-bs-toggle="collapse" data-bs-target="#transaksi">
+				<img src="{{ asset('icon/data.png') }}" alt=".." class="me-2" style="width: 20px; height: 20px;">
+				<span>Transaksi</span>
+				<i class="fa-solid fa-chevron-down"></i>
+			</div>
+			<div class="collapse w-100 {{ Request::is('barangs') || Request::is('suppliers-users') || Request::is('barang-masuks') || Request::is('pinjams-kembalis*') ? 'show' : '' }}" id="transaksi">
+				<div class="sub-menu">						
+					<a class="{{ Request::is('suppliers-users*') ? 'active' : '' }}" href="/siswas">Siswa</a>
+					<a href="/pinjams-kembalis" class="{{ Request::is('pinjams-kembalis*') ? 'active' : '' }}">Pinjam & Kembali</a>
+				</div>
+			</div>
+		</div>
+		<div class="sidebar-menu d-flex flex-column align-items-start">
+			<div class="menu" data-bs-toggle="collapse" data-bs-target="#laporan">
+				<img src="{{ asset('icon/data.png') }}" alt=".." class="me-2" style="width: 20px; height: 20px;">
+				<span>Laporan</span>
+				<i class="fa-solid fa-chevron-down"></i>
+			</div>
+			<div class="collapse w-100 {{ Request::is('barangs') || Request::is('suppliers-users') || Request::is('barang-masuks') || Request::is('pinjams-kembalis*') ? 'show' : '' }}" id="laporan">
 				<div class="sub-menu">						
 					<a class="{{ Request::is('suppliers-users*') ? 'active' : '' }}" href="/siswas">Siswa</a>
 					<a href="/pinjams-kembalis" class="{{ Request::is('pinjams-kembalis*') ? 'active' : '' }}">Pinjam & Kembali</a>
